1d93639644ce9c9e5ab88acd18c6d76bf7be28fd
Bill Hefty <billhefty@gmail.com>
2017-02-19 13:54:23 -0600

    Add auth-lock creation to route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Router, Route } from 'react-router'
+import AuthService from './utils/AuthService'
 
 import App from './modules/App/'
 
+const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID
+const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN
+
+const auth = new AuthService(AUTH0_CLIENT_ID, AUTH0_DOMAIN)
+
 const Routes = (props) => (
   <Router {...props}>
-    <Route path='/' component={App}>
+    <Route path='/' component={App} auth={auth}>
 
     </Route>
   </Router>
